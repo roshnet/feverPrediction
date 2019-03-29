ed90830f2e2f4c4f3455effb14e80524efe38973
Roshan Sharma <reprogram46@gmail.com>
2019-03-30 01:55:54 +0530

    predict.py: Add sample code

---
+++ code/predict.py
@@ -0,0 +1,38 @@
+
+from sklearn import tree
+import pandas as pd
+import numpy as np
+
+clf = tree.DecisionTreeClassifier()
+
+
+# Data that is going to be retrieved from sensors is mock here.
+df = pd.read_csv('symptoms.csv')
+
+FEATURES = df.iloc[:,:-1]
+LABELS = df.iloc[:,-1]
+
+PARAM_COUNT = len(df.columns) - 1
+# Exclude the last column for being label.
+
+clf.fit(FEATURES, LABELS)
+
+TEST_SET = np.tile(0, PARAM_COUNT)
+
+
+for idx in range(PARAM_COUNT):
+    TEST_SET[idx] = input('{param}: '
+                          .format(param=df.columns[idx]))    
+
+
+result = clf.predict([TEST_SET])
+
+
+OUTPUT = {}
+OUTPUT[0] = False
+OUTPUT[1] = True
+
+print('Fever present: {res}'
+      .format(res=OUTPUT[result[0]]))
+
+
